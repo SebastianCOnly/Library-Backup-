bc3a66ecfc634bb8f6df77545ffa0b1dd2058429
tasaTemp <tsamari@csu.fullerton.edu>
2024-07-23 23:14:28 -0700

    tests.py

--- TestSite-main/library/tests.py
+++ TestSite-main/library/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+# Documentation for pytest-django: https://pytest-django.readthedocs.io/en/latest/index.html
+# Marking test functions in Pytest: https://docs.pytest.org/en/stable/how-to/mark.html
 
-# Create your tests here.
+import pytest
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+# Functionality Testing
+
+# Checks if valid user account creation & login is valid
+@pytest.mark.django_db # Requesting database access
+def test_user_account_valid():
+    # WORDS
