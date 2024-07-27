2a3b432a384ecc3a29281855db4ccff3d25301de
Kramos-9173 <60204681+Kramos-9173@users.noreply.github.com>
2024-07-27 05:17:48 -0700

    Comments

--- TestSite-main/urls.py
+++ TestSite-main/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ensures that any files in the MEDIA_ROOT directory can be accessed via URLs that start with MEDIA_URL.
