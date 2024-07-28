9d318276ca3c9db0ecbd5420ddf032c0b93107d6
Kramos-9173 <60204681+Kramos-9173@users.noreply.github.com>
2024-07-28 08:57:23 -0700

    Delete TestSite-main/library/account_creation.py

--- TestSite-main/library/account_creation.py
+++
@@ -1,7 +0,0 @@
-# Official_LMS/library/account_creation.py
-from django.contrib.auth.forms import UserCreationForm
-
-class UserCreationFormWithFullName(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'full_name', 'password1', 'password2')
