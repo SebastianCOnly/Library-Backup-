d451f865249f57060e8a6b6a9ff1185c7dc6f4a8
tasaTemp <114367768+tasaTemp@users.noreply.github.com>
2024-07-27 19:43:18 -0700

    Update conftest.py

--- TestSite-main/conftest.py
+++ TestSite-main/conftest.py
@@ -7,21 +7,34 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db import connection
+from typing import Dict
+from unittest.mock import Mock, patch
 
 client = Client()
 
 # Custom fixtures
 
-# Enables database access to all tests without
-# the need to invoke '@pytest.mark.django_db'
+# Enables database access for all tests without the need to invoke '@pytest.mark.django_db'
 @pytest.fixture
 def enable_db_access_for_all_tests(db):
     pass
 
+# Creates a dummy user for testing purposes
 @pytest.fixture
 def user(django_user_model):
-    username = "ButtermilkUser"
-    password = "myPass1234"
-    dummyUser = django_user_model.objects.create_user(username=username, 
-                                                 password=password)
+    username = 'ButtermilkUser'
+    password = 'myPass1234'
+    emailAddr = 'realEmailHere@CSUFakeMail.com'
+    dummyUser = django_user_model.objects.create_user(
+                                                 username=username, 
+                                                 password=password,
+                                                 email=emailAddr)
     return dummyUser
+
+@pytest.fixture
+def titleOne():
+    return 'The Lightning Thief'
+
+@pytest.fixture
+def titleTwo():
+    return 'riordan'
